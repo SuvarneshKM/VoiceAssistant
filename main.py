b2d2f854c611696abf6f721a09d19ef486cc9d7e
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2021-02-13 15:33:33 +0530

    speak function

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time 
+import pyttsx3
+import playsound
+import speech_recognition as sr
+import pytz
+import subprocess
+
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+MONTHS = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+DAY_EXTENSIONS = ["nd", "rd", "th", "st"]
+
+def speak(text):
+	engine = pyttsx3.init()
+	engine.say(text)
+	engine.runAndWait()
+
+def get_audio():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+		said = ""
+
+		try:
+			said = r.recognize_google(audio)
+			print(said)
+		except Exception as e:
+			print("Exception: " + str(e))
+
+	return said.lower()		
 
