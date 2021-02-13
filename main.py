c3cfa1bf28079891e8c4b635fa10479a8b398cbe
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2021-02-13 15:35:29 +0530

    get date

--- main.py
+++ main.py
@@ -90,3 +90,56 @@ def get_events(day, service):
 
 			speak(event["summary"] + " at " + start_time)
 
+
+def get_date(text):
+	text = text.lower()
+	today = datetime.date.today()
+
+	if text.count("today") > 0:
+		return today
+
+	day = -1
+	day_of_week = -1
+	month = -1
+	year = today.year
+
+	for word in text.split():
+		if word in MONTHS:
+			month = MONTHS.index(word) + 1
+
+		elif word in DAYS:
+			day_of_week = DAYS.index(word)
+
+		elif word.isdigit():
+			day = int(word)
+
+		else:
+			for ext in DAY_EXTENSIONS:
+				found = word.find(ext)
+				if found > 0:
+					try:
+						day = int(word[:found])
+					except:
+						pass		
+
+	if month < today.month and month != -1:
+		year = year + 1
+
+	if day < today.day and month == -1 and day != -1:
+		month = month + 1 	
+
+	if month == -1 and day == -1 and day_of_week != -1:
+		current_day_of_week = today.weekday()
+		dif = day_of_week - current_day_of_week
+
+		if dif < 0:
+			dif += 7
+			if text.count("next") >= 1:
+				dif += 7
+
+		return today + datetime.timedelta(dif)
+	
+	if month == -1 or day == -1:
+		return None
+	return datetime.date(month=month, day=day, year=year)	
+
